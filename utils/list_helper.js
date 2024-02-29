3a0b1437cd6be6c2c80d2644ba4f266cc362da67
Sean <seanhoyeonjin@gmail.com>
2024-02-29 16:04:43 -0500

    finished exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const initialValue = 0;
+  const totalLikes = blogs.reduce((accumulator, item) => {
+    return (accumulator += item.likes);
+  }, 0);
+
+  return totalLikes;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
