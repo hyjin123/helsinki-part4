3eaa9885bf07c9ebb553e8fbb826f21d9594259a
Sean <seanhoyeonjin@gmail.com>
2024-03-04 21:56:39 -0500

    added an error handling middleware in the utils folder

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const { info, error } = require("./utils/logger");
 const usersRouter = require("./controllers/users");
 const blogsRouter = require("./controllers/blogs");
 const cors = require("cors");
+
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
@@ -24,8 +27,12 @@ mongoose
 app.use(cors());
 app.use(express.static("dist"));
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use("/api/users", usersRouter);
 app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
--- controllers/users.js
+++ controllers/users.js
@@ -13,6 +13,19 @@ usersRouter.get("/", async (request, response) => {
 usersRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
 
+  // if no username or password, return an error
+  if (!username || !password) {
+    return response
+      .status(400)
+      .json({ error: "username or password is missing" });
+  }
+
+  // password must be at least 3 lengths long
+  if (password.length < 3) {
+    return response
+      .status(400)
+      .json({ error: "password must be at least 3 characters long" });
+  }
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
 
