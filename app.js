14806d4e29e56b4fd8c06be4abf0bc0666a207b1
Sean <seanhoyeonjin@gmail.com>
2024-03-05 15:29:50 -0500

    finished exercise 4.18

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 require("express-async-errors");
 const { MONGODB_URI } = require("./utils/config");
 const { info, error } = require("./utils/logger");
+
 const usersRouter = require("./controllers/users");
 const blogsRouter = require("./controllers/blogs");
+const loginRouter = require("./controllers/login");
+
 const cors = require("cors");
 
 const middleware = require("./utils/middleware");
@@ -31,6 +34,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/users", usersRouter);
 app.use("/api/blogs", blogsRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
